609b994841af844c664e55511e0268832428d9e2
Michael Villarruel <alejovillarruel@gmail.com>
2022-04-12 17:41:12 -0500

    feat: solve test__score_function__returns_the_correct_score__when_have_a_spare

--- bowling_game.py
+++ bowling_game.py
@@ -11,6 +11,15 @@ class Game:
 
     def score(self) -> int:
         score: int = 0
-        for roll in self.rolls:
-            score += roll
+        roll_index = 0
+
+        for frame in range(10):
+            score += (self.rolls[roll_index] + self.rolls[roll_index+1])
+            if self.is_a_spare(roll_index):
+                score += self.rolls[roll_index+2]
+            roll_index += 2
+
         return score
+
+    def is_a_spare(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index+1] == 10
