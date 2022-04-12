cbde899502c637b71436a2a9dbac17fd10581618
Michael Villarruel <alejovillarruel@gmail.com>
2022-04-12 17:09:23 -0500

    test: test__score_function__returns_twenty__when_every_roll_scores_one_pin

--- bowling_game_tests.py
+++ bowling_game_tests.py
@@ -8,3 +8,11 @@ def test__score_function__returns_zero__when_every_roll_scores_zero_pins():
         game.roll(0)
 
     assert game.score() == 0
+
+def test__score_function__returns_twenty__when_every_roll_scores_one_pin():
+    game = Game()
+
+    for i in range(20):
+        game.roll(1)
+
+    assert game.score() == 20
