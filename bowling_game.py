062be37ca89a0d8225ea5337e8a3d3b8f64e8fd4
Michael Villarruel <alejovillarruel@gmail.com>
2022-04-12 18:04:43 -0500

    feat: solve test__score_function__returns_the_correct_score__when_have_a_strike

--- bowling_game.py
+++ bowling_game.py
@@ -17,9 +17,14 @@ class Game:
             score += (self.rolls[roll_index] + self.rolls[roll_index+1])
             if self.is_a_spare(roll_index) and roll_index < len(self.rolls) - 2:
                 score += self.rolls[roll_index+2]
+            if self.is_a_strike(roll_index):
+                score += (self.rolls[roll_index+2] + self.rolls[roll_index+3])
             roll_index += 2
 
         return score
 
     def is_a_spare(self, roll_index):
-        return self.rolls[roll_index] + self.rolls[roll_index+1] == 10
+        return self.rolls[roll_index] + self.rolls[roll_index+1] == 10 and self.rolls[roll_index] != 10
+    
+    def is_a_strike(self, roll_index):
+        return self.rolls[roll_index] == 10
